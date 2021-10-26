e81013b5db01326eac13a4f61c44c5158cc4863e
swaggp <mutebinuhu1@gmail.com>
2021-10-26 08:36:46 +0300

    my initial commit

---
+++ mysite/blog/models.py
@@ -0,0 +1,21 @@
+from datetime import time
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    auther = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    published_at = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_at = timezone.now()
+        self.save()
+
+    def __str_(self):
+        return self.title
+    # Create your models here.
